4d8e5f23c63dd2870c794e18159c7fdd8df14972
Muneera <muneera@Muneeras-MacBook-Air.local>
2019-02-10 18:16:26 +0300

    Muneera Project

---
+++ scripts/example.js
@@ -0,0 +1,38 @@
+'use strict'
+
+module.exports = (robot) => {
+  
+  // 1 Simple greeting 
+  // robot.hear(/hi/i, (res) => {
+  //   res.send('hello there')
+  // })
+  
+  // 2 Asking about the class
+  // robot.hear(/class today/i, (res) => {
+  //   let date1 = new Date()
+  //   let day1 = date1.getDay() // store number of the day in day1
+  //   if (day1 == 1 || day1 == 3){ //1 = mon and 3 = wed
+  //     res.send('Yesss')
+  //   }else {
+  //     res.send('Noooooo wooohooo')
+  //   }
+  // })
+
+  // 3 Random value returned 
+  // const color = ['Red!', 'White?', 'Black?']
+  // robot.respond(/guess the color/i, (res) => {
+  //  res.send(res.random(color))
+  // })
+
+  // 4 Reply to the member
+  // robot.respond(/love me/i, (response) => {
+  //   response.reply('I Do!')
+  //  //setInterval(() => response.reply('a looot'), 1)
+  // })
+
+  // 5 Schedule a message
+
+
+  // 6 Reaction
+ 
+}
